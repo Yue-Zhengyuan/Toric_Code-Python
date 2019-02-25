5c5dc8b4ce5fcff464234f46ee1547f7aebbf508
Yue Zhengyuan <yuezy1997@icloud.com>
2019-02-25 12:41:05 +0800

    removed Ising model

--- test.py
+++ test.py
@@ -1,14 +1,14 @@
 # 
-#   main_mps_ising.py
+#   main_mps.py
 #   Toric_Code-Python
-#   apply the gates and MPO to MPS (Ising Model)
+#   apply the gates and MPO to MPS
 #
 #   created on Feb 18, 2019 by Yue Zhengyuan
 #
 
 import numpy as np
-import ising_gates
-import ising_para_dict as p
+import gates
+import para_dict as p
 import lattice as lat
 import mps
 import mpo
@@ -29,57 +29,102 @@ para['hz'] = 0
 # mode = sys.argv[1]
 mode = '2'
 
-# create local sx operator MPO
+# create directory to save result
+# create folder to store results
+# get system time
+current_time = datetime.datetime.now()
+result_dir = 'result_mps'
+result_dir += '_' + str(current_time.year)
+result_dir += '-' + str(current_time.month)
+result_dir += '-' + str(current_time.day)
+result_dir += '_' + str(current_time.hour)
+result_dir += '_' + str(current_time.minute)
+os.makedirs(result_dir, exist_ok=True)
+
+# create string operator MPO (S)
 # labelling: [site][L vir leg, R vir leg, U phys leg, D phys leg]
 # len(MPO): number of sites
-sx_op = []
+str_op = []
 for i in range(p.n):
-    sx_op.append(np.zeros((1,2,2,1), dtype=complex))
-    sx_op[i][0,:,:,0] = p.iden
-coord = [2,2]
-num = coord[1] * para['nx'] + coord[0]
-sx_op[num][0,:,:,0] = p.sx
-
-# |+z>
-psi_up = []
+    str_op.append(np.zeros((1,2,2,1), dtype=complex))
+# string along x
+bond_on_str = [(8,10,'r'),(9,10,'r'),(10,10,'r'),(11,10,'r'),(12,10,'r')]
+# string along y
+# bond_on_str = [(10,8,'d'),(10,9,'d'),(10,10,'d'),(10,11,'d'),(10,12,'d')]
+# convert coordinate to unique number
+bond_list = []
+for bond in bond_on_str:
+    bond_list.append(lat.lat(bond[0:2],bond[2],para['nx']))
 for i in range(p.n):
-    psi_up.append(np.zeros((1,2,1), dtype=complex))
-    psi_up[i][0,0,0] = 1.0
+    if i in bond_list:
+        str_op[i][0,:,:,0] = p.sx
+    else:
+        str_op[i][0,:,:,0] = p.iden
 
-result = copy.copy(psi_up)
+# create Ising ground state |psi>
+psi = []
+for i in range(p.n):
+    psi.append(np.zeros((1,2,1), dtype=complex))
+    psi[i][0,0,0] = 1.0
 
-start = time.perf_counter()
+# save parameter of current running
+with open(result_dir + '/parameters.txt', 'w+') as file:
+    file.write(json.dumps(p.para))  # use json.loads to do the reverse
+    file.write('\nSites on String\n')
+    file.write(str(bond_on_str))    # coordinate
+    file.write(str(bond_list))      # bond number
 
-# exp(+iHt) S exp(-iHt) |psi_S>
+# exp(+iHt) S exp(-iHt) |psi>
 # adiabatic continuation of string operator
 if mode == '0':
-    ttotal = para['ttotal']
-    tstep = para['tau']
-    stepNum = int(ttotal/tstep + (1e-9 * (ttotal/tstep)))
-    # exp(-iHt) |psi_S>
-    for hx in np.linspace(0, p.para['hx'], num=stepNum, dtype=float):
+    stepNum = int(p.para['ttotal'] / p.para['tau'])
+    # exp(-iHt) |psi>
+    for hx in np.linspace(0, -p.para['hx'], num=stepNum, dtype=float):
         para['hx'] = hx
-        gateList = ising_gates.makeGateList(sx_op, para)
-        result = mps.gateTEvol(result, gateList, para['tau'], para['tau'], cutoff, bondm)
-    # S exp(-iHt) |psi_S>
-    result = mps.applyMPOtoMPS(sx_op, result, cutoff, bondm)
-    # exp(+iHt) S exp(-iHt) |psi_S>
-    para['J'] *= -1
-    for hx in reversed(np.linspace(0, p.para['hx'], num=stepNum, dtype=float)):
-        para['hx'] = -hx
-        gateList = ising_gates.makeGateList(sx_op, para)
-        result = mps.gateTEvol(result, gateList, para['tau'], para['tau'], cutoff, bondm)
-    mps.printdata(result)
+        gateList = gates.makeGateList(str_op, para)
+        psi = mps.gateTEvol(psi, gateList, para['tau'], para['tau'], cutoff, bondm)
+    # S exp(-iHt) |psi>
+    psi = mps.applyMPOtoMPS(str_op, psi, cutoff, bondm)
+    # exp(+iHt) S exp(-iHt) |psi>
+    for hx in reversed(np.linspace(0, -p.para['hx'], num=stepNum, dtype=float)):
+        para['hx'] = hx
+        gateList = gates.makeGateList(str_op, para)
+        for g in gateList:
+            g.gate = np.conj(g.gate)
+        psi = mps.gateTEvol(psi, gateList, para['tau'], para['tau'], cutoff, bondm)
+    mps.save_to_file(psi, result_dir + '/adiab_psi.txt')
+
+# quasi-adiabatic continuation of string operator
+elif mode == '1':
+    stepNum = 4
+    # exp(-iHt) |psi>
+    iter_list = np.linspace(0, -p.para['hx'], num=stepNum+1, dtype=float)
+    iter_list = np.delete(iter_list, 0)
+    for hx in iter_list:
+        para['hx'] = hx
+        gateList = gates.makeGateList(str_op, para)
+        psi = mps.gateTEvol(psi, gateList, para['ttotal']/stepNum, para['tau'], cutoff, bondm)
+    # S exp(-iHt) |psi>
+    psi = mps.applyMPOtoMPS(str_op, psi, cutoff, bondm)
+    # exp(+iHt) S exp(-iHt) |psi>
+    for hx in reversed(iter_list):
+        para['hx'] = hx
+        gateList = gates.makeGateList(str_op, para) # create exp(-iHt)
+        for g in gateList:
+            g.gate = np.conj(g.gate)                # convert to exp(+iHt)
+        psi = mps.gateTEvol(psi, gateList, para['ttotal']/stepNum, para['tau'], cutoff, bondm)
+    mps.save_to_file(psi, result_dir + '/quasi_psi.txt')
 
 # no-field Heisenberg evolution of string operator
 elif mode == '2':
-    # test one step of time evolution
-    # exp(-iHt) |psi_S>
-    para['hx'] = 1.0
-    gateList = ising_gates.makeGateList(sx_op, para)
-    result = mps.gateTEvol(result, gateList, para['tau'], para['tau'], cutoff, bondm)
-    mps.printdata(result)
-
-finish = time.perf_counter()
-elapsed = finish - start
-print("Elapsed time:", elapsed, "s\n")
+    # exp(-iHt) |psi>
+    para['hx'] = 0.0
+    gateList = gates.makeGateList(str_op, para)
+    psi = mps.gateTEvol(psi, gateList, para['ttotal'], para['tau'], cutoff, bondm)
+    # S exp(-iHt) |psi>
+    psi = mps.applyMPOtoMPS(str_op, psi, cutoff, bondm)
+    # exp(+iHt) S exp(-iHt) |psi>
+    for g in gateList:
+        g.gate = np.conj(g.gate)
+    psi = mps.gateTEvol(psi, gateList, para['ttotal'], para['tau'], cutoff, bondm)
+    mps.save_to_file(psi, result_dir + '/no_field_psi.txt')
